f7b3cebe0a9ec116466d4793cf7ee8059161008f
Wa-542 <mkm211088@gmail.com>
2025-08-21 13:53:29 +0700

    SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -19,3 +19,15 @@ WHERE Shippers.ShipperID = Orders.Shipvia
 SELECT CompanyName, OrderID
 FROM Orders JOIN Shippers
 ON Shippers.ShipperID = Orders.Shipvia
+
+--จงแสดงข้อมูลหมายเลขใบสั่งซื้อและชื่อบริษัทขนส่งสินค้าของใบสั่งซื้อหมายเลข 10275
+--• Cartesian Product
+SELECT CompanyName, OrderID
+FROM Orders, Shippers
+WHERE Shippers.ShipperID = Orders.Shipvia
+AND OrderID = 10275
+--• Join Operator
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID=Orders.Shipvia
+WHERE OrderID=10275
