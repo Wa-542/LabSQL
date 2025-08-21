d3fe0cd97b139e8fbc73b42d81decda7ca8786d4
Wa-542 <mkm211088@gmail.com>
2025-08-21 13:43:03 +0700

    SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,4 +1,8 @@
 --แสดงชื่อประเภทสินค้า ชื่อสินค้า และ ราคาสินค้า
 Select CategoryName, ProductName, UnitPrice
 From Products as P, Categories as C
-On P.CategoryID=C.CagetgoryID
+WHERE P.CategoryID = C.CategoryID
+--แสดงชื่อประเภทสินค้า ชื่อสินค้า และ ราคาสินค้า
+Select CategoryName, ProductName,UnitPrice
+From Products Join Categories
+On Products.CategoryID=Categories.C.CategoryID
