dd33d9f162de8ff4ab17390844aa4a23a5b55ef4
Wa-542 <mkm211088@gmail.com>
2025-08-21 13:50:28 +0700

    SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -2,7 +2,20 @@
 Select CategoryName, ProductName, UnitPrice
 From Products as P, Categories as C
 WHERE P.CategoryID = C.CategoryID
+AND CategoryName = 'seafood'
+
 --แสดงชื่อประเภทสินค้า ชื่อสินค้า และ ราคาสินค้า
-Select CategoryName, ProductName,UnitPrice
-From Products Join Categories
-On Products.CategoryID = C.CategoryID
+Select CategoryName, ProductName, UnitPrice
+From Products as P, Categories as C
+on  P.CategoryID = C.CategoryID
+AND CategoryName = 'seafood'
+
+--จงแสดงข้อมูลหมายเลขใบสั่งซื้อและชื่อบริษัทขนส่งสินค้า
+--•Cartesian Product
+SELECT CompanyName, OrderID
+FROM Orders, Shippers
+WHERE Shippers.ShipperID = Orders.Shipvia
+--•Join Operator
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID = Orders.Shipvia
