5c7d295ae0c5d8045330f4873089c58fe51205f7
Wa-542 <mkm211088@gmail.com>
2025-08-21 13:43:56 +0700

    SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -5,4 +5,4 @@ WHERE P.CategoryID = C.CategoryID
 --แสดงชื่อประเภทสินค้า ชื่อสินค้า และ ราคาสินค้า
 Select CategoryName, ProductName,UnitPrice
 From Products Join Categories
-On Products.CategoryID=Categories.C.CategoryID
+On Products.CategoryID = C.CategoryID
