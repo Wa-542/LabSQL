c79e467909b24b4de68830a9b8ee26bdb8124a76
Wa-542 <mkm211088@gmail.com>
2025-08-21 14:06:48 +0700

    SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -31,3 +31,31 @@ SELECT CompanyName, OrderID
 FROM Orders JOIN Shippers
 ON Shippers.ShipperID=Orders.Shipvia
 WHERE OrderID=10275
+
+--ต้องการรหัสสินค้า ชื่อสินค้า บริษัทผู้จำหน่าย ประเทศ
+SELECT 
+    p.ProductID AS รหัสสินค้า,
+    p.ProductName AS ชื่อสินค้า,
+    s.CompanyName AS บริษัทผู้จำหน่าย,
+    s.Country AS ประเทศ
+FROM Products p
+JOIN Suppliers s ON p.SupplierID = s.SupplierID;
+
+--ต้องการรหัสพนังงาน ชื่อพนักงาน รหัสใบสั่งซื้อืี่เกี่ยวข้อง เรียงตามลำดับรหัสพนักงาน
+SELECT 
+    e.EmployeeID AS รหัสพนักงาน,
+    (e.FirstName + ' ' + e.LastName) AS ชื่อพนักงาน,
+    o.OrderID AS รหัสใบสั่งซื้อ
+FROM Employees e
+JOIN Orders o ON e.EmployeeID = o.EmployeeID
+ORDER BY e.EmployeeID;
+
+--ต้องการรหัสสินค้า ชื่อสินค้า เมือง และประประเทศของบริษัทผู้จำหน่าย
+SELECT 
+    p.ProductID AS รหัสสินค้า,
+    p.ProductName AS ชื่อสินค้า,
+    s.City AS เมือง,
+    s.Country AS ประเทศ
+FROM Products p
+JOIN Suppliers s ON p.SupplierID = s.SupplierID;
+
